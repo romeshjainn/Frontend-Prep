ecd7ed3c38c989f2b701e75580759bab6c597e67
romeshjainn <romeshjainofficial@gmail.com>
2023-08-10 21:59:13 +0530

    undo done

--- undo multi/script.js
+++ undo multi/script.js
@@ -6,26 +6,21 @@ let undo = document.getElementById("undo");
 let redo = document.getElementById("redo");
 let count = -1;
 let store = [];
-let varStore = [];;
+let varStore = [];
+let not = false;
 undo.onclick = function () {
-    
+  if (!not) {
+    const number = numberInput.value;
+    const email = emailInput.value;
+    const name = nameInput.value;
+    store.push(email, number, name);
+    not = true;
+  }
+  console.log(store);
 
-    if (store.length === 0) {
-      store.push(emailInput.value, numberInput.value, nameInput.value);
-      console.log(store);
-    }
-    //   const number = numberInput.value;
-    //   const email = emailInput.value;
-    //   const name = nameInput.value;
-    //   store.push(email, number, name);
-    //   const arr = store;
-    //   console.log(arr);
-    
-    
-    count++;
-    varStore.push(emailInput, numberInput, nameInput);
-    varStore[count].value = " ";
-   
+  count++;
+  varStore.push(emailInput, numberInput, nameInput);
+  varStore[count].value = " ";
 };
 
-console.log(arr)
+console.log(arr);
