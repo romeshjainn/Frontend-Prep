15961dfa2646e3d07b34e78d3dac4e7ada8cd13f
romeshjainn <romeshjainofficial@gmail.com>
2023-08-10 21:51:14 +0530

    update

--- undo multi/script.js
+++ undo multi/script.js
@@ -1,37 +1,31 @@
+let nameInput = document.getElementById("name");
+let numberInput = document.getElementById("number");
+let emailInput = document.getElementById("email");
+
 let undo = document.getElementById("undo");
 let redo = document.getElementById("redo");
 let count = -1;
 let store = [];
-let store2 = [];;
+let varStore = [];;
 undo.onclick = function () {
-  let nameInput = document.getElementById("name").value;
-  let numberInput = document.getElementById("number");
-  let emailInput = document.getElementById("email");
+    
 
+    if (store.length === 0) {
+      store.push(emailInput.value, numberInput.value, nameInput.value);
+      console.log(store);
+    }
+    //   const number = numberInput.value;
+    //   const email = emailInput.value;
+    //   const name = nameInput.value;
+    //   store.push(email, number, name);
+    //   const arr = store;
+    //   console.log(arr);
     
-    // let name = nameInput.value;
-    // let number = numberInput.value;
-    // let email = emailInput.value;
-    // store.push(email, number, name);
+    
     count++;
-    
-    store.push(emailInput, numberInput, nameInput);
-    store[count].value = " ";
-    // for (let i = 0; i < store.length; i++){
-    //     store[i].value = "2";
-    // }
-    // store[1] = "55";
-    // numberInput.value = "55";
-    
-    // name.value() = " ";
-//   let remove = store.length - 1;
-    // document.getElementById("email").v?alue = "";
-    
-//   document.getElementById("number").value = "";
-//   document.getElementById("name").value = "";
-  console.log(store);
+    varStore.push(emailInput, numberInput, nameInput);
+    varStore[count].value = " ";
+   
 };
-// redo.onclick = function () {
-//     email.value = store.length - 1;
-//     console.log(email)
-// }
+
+console.log(arr)
