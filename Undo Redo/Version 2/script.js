519c85aebe1581e54389efe937887ed69391253f
romeshjainn <romeshjainofficial@gmail.com>
2023-08-11 16:12:46 +0530

    optimized the undo redo v2

--- Undo Redo/Version 2/script.js
+++ Undo Redo/Version 2/script.js
@@ -1,42 +1,31 @@
 let nameInput = document.getElementById("name");
 let numberInput = document.getElementById("number");
 let emailInput = document.getElementById("email");
-let inputArr = [];
-inputArr.push(emailInput, numberInput, nameInput);
+let inputArr = [emailInput, numberInput, nameInput];
 
 let undo = document.getElementById("undo");
 let redo = document.getElementById("redo");
 let count = -1;
 let store = [];
-let varStore = [];
 let not = false;
+
 undo.onclick = function () {
   if (!not) {
-    const number = numberInput.value;
-    const email = emailInput.value;
-    const name = nameInput.value;
-    store.push(email, number, name);
+    store.push(emailInput.value, numberInput.value, nameInput.value);
     not = true;
   }
-  console.log(store);
 
   count++;
-  varStore.push(emailInput, numberInput, nameInput);
-  varStore[count].value = " ";
+  let currentInputs = [emailInput, numberInput, nameInput];
+  currentInputs[count].value = " ";
 };
 
 redo.onclick = function () {
   let storeCount = store.length - 1;
   let inputCount = inputArr.length - 1;
-  
+
   inputArr[inputCount].value = store[storeCount];
-  
+
   store.pop();
   inputArr.pop();
-
-    console.log(store);
-    console.log(inputArr);
 };
-
-
-// console.log(arr);
