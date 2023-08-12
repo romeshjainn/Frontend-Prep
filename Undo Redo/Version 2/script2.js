2f4fb07f441439e140c8b5f02373823422e2d02b
romeshjainn <romeshjainofficial@gmail.com>
2023-08-12 10:57:30 +0530

    update

--- Undo Redo/Version 2/script2.js
+++ Undo Redo/Version 2/script2.js
@@ -1,10 +1,33 @@
 let nameInput = document.getElementById("name");
 let numberInput = document.getElementById("number");
 let emailInput = document.getElementById("email");
-let inputArr = [];
 
 let undo = document.getElementById("undo");
 let redo = document.getElementById("redo");
+let arrInput = [emailInput, numberInput, nameInput];
 let store = [];
-let not = false;
 let count = 0;
+let not = false;
+let not2 = false;
+
+
+undo.onclick = function () {
+  if (!not2) {
+    let input = nameInput.value;
+    let number = numberInput.value;
+    let email = emailInput.value;
+    store.push(input, number, email);
+      not2 = true;
+}
+
+if (!not) {
+    arrInput[count].value = " ";
+    count++;
+  }
+};
+
+redo.onclick = function () {
+  store.reverse();
+  count--;
+  arrInput[count].value = store[count];
+};
