c5978a24175d20b06b74e4869109a250d29a5024
romeshjainn <romeshjainofficial@gmail.com>
2023-08-12 08:27:46 +0530

    update

---
+++ Undo Redo/Version 3/script.js
@@ -0,0 +1,9 @@
+let nameInput = document.getElementById("name");
+let numberInput = document.getElementById("number");
+let emailInput = document.getElementById("email");
+let undo = document.getElementById("undo");
+let redo = document.getElementById("redo");
+
+let store = [];
+let not = false;
+
