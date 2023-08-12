1d28a624fd867b1574dc432222065bd514be1595
romeshjainn <romeshjainofficial@gmail.com>
2023-08-12 09:58:14 +0530

    test v2

---
+++ Undo Redo/Version 2/script2.js
@@ -0,0 +1,10 @@
+let nameInput = document.getElementById("name");
+let numberInput = document.getElementById("number");
+let emailInput = document.getElementById("email");
+let inputArr = [];
+
+let undo = document.getElementById("undo");
+let redo = document.getElementById("redo");
+let store = [];
+let not = false;
+let count = 0;
