7bf01c4c29aa851d95720d94ff0351c79daa9b9d
romeshjainn <romeshjainofficial@gmail.com>
2023-08-12 08:53:54 +0530

    update

--- Undo Redo/Version 3/script.js
+++ Undo Redo/Version 3/script.js
@@ -1,9 +1,24 @@
-let nameInput = document.getElementById("name");
-let numberInput = document.getElementById("number");
-let emailInput = document.getElementById("email");
-let undo = document.getElementById("undo");
-let redo = document.getElementById("redo");
+  // let inputArr = document.querySelectorAll("input");
+  // let undo = document.getElementById("undo");
+  // let redo = document.getElementById("redo");
+  // let store = {};
+  // let focusedInput = null;
 
-let store = [];
-let not = false;
+  // inputArr.forEach(function (input) {
+  //   input.addEventListener("input", function (event) {
+  //     if (focusedInput === input) {
+  //       store[input.id] = store[input.id] || [];
+  //       store[input.id].push(event.data); 
+  //       console.log(store); 
+  //     }
+  //   });
+
+  //   input.addEventListener("focus", function () {
+  //     focusedInput = input;
+  //   });
+
+  //   input.addEventListener("blur", function () {
+  //     focusedInput = null;
+  //   });
+  // });
 
