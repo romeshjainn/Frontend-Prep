436f0a7cc67101fe8de1130471e5dbb0479525b2
romeshjainn <romeshjainofficial@gmail.com>
2023-08-13 13:40:06 +0530

    added prpper comments and working so far

--- Snake & Ladders/script.js
+++ Snake & Ladders/script.js
@@ -1,26 +1,36 @@
+// Create pawn element
 let pawn = document.createElement("div");
 pawn.className = "pawn";
 
+// Create container element
 let container = document.createElement("div");
-let button = document.createElement("button");
-let start = document.createElement("button");
 container.className = "container";
 document.body.appendChild(container);
+
+// Create buttons for controlling the game
+let button = document.createElement("button");
+let start = document.createElement("button");
+start.innerText = "Start";
+button.innerText = "Roll Dice";
 document.body.appendChild(button);
 document.body.appendChild(start);
-start.innerText = "start";
-button.innerText = "button";
+
+// Set up grid center layout
 document.body.className = "gridCenter";
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
-let boxes;
+
+// Set up store area
 let store = document.createElement("div");
 container.appendChild(store);
 
-
+// Create and initialize variables
+let boxes;
 let boxCount = 0;
 let arr = [];
 let count = 0;
+
+// Create the grid of boxes
 for (let i = 100; i > 0; i--) {
   boxes = document.createElement("div");
   container.appendChild(boxes);
@@ -35,6 +45,7 @@ for (let i = 100; i > 0; i--) {
   boxCount = 100 - i + 1;
   arr.push(boxes);
 
+  // Apply specific styling for certain boxes
   if (
     boxCount <= 10 ||
     (boxCount > 20 && boxCount <= 30) ||
@@ -52,7 +63,11 @@ for (let i = 100; i > 0; i--) {
     }
   }
 }
+
+// Append the pawn to a specific box
 arr[99].appendChild(pawn);
+
+// Create and set up snake and ladder images
 let snake1 = document.createElement("img");
 let snake2 = document.createElement("img");
 let snake3 = document.createElement("img");
@@ -64,96 +79,45 @@ let ladder2 = document.createElement("img");
 let ladder3 = document.createElement("img");
 let ladder4 = document.createElement("img");
 
+// Set up snake and ladder image sources
 snake1.className = "s1";
-snake1.src = "img/s1.png"
-snake2.src = "img/s2.png"
+snake1.src = "img/s1.png";
+snake2.src = "img/s2.png";
 snake3.src = "img/s3.png";
-snake4.src = "img/s4.png"
-snake5.src = "img/s5.png"
-snake6.src = "img/s6.png"
+snake4.src = "img/s4.png";
+snake5.src = "img/s5.png";
+snake6.src = "img/s6.png";
+ladder1.src = "img/l1.png";
+ladder2.src = "img/l2.png";
+ladder3.src = "img/l3.png";
+ladder4.src = "img/l4.png";
 
-ladder1.src = "img/l1.png"
-ladder2.src = "img/l2.png"
-ladder3.src = "img/l3.png"
-ladder4.src = "img/l4.png"
-
+// Append images to specific boxes
 arr[12].appendChild(snake1);
 arr[87].appendChild(snake2);
 arr[63].appendChild(snake3);
 arr[19].appendChild(snake4);
 arr[50].appendChild(snake5);
 arr[58].appendChild(snake6);
-
 arr[83].appendChild(ladder1);
 arr[16].appendChild(ladder2);
 arr[44].appendChild(ladder3);
 arr[20].appendChild(ladder4);
 
-
-
-
-
-count = arr.length - 1;
-// console.log("count", count)
+// Set up dice roll functionality
 let curBox = 99;
-
-
-
 button.onclick = function () {
   let randomNumber = Math.floor(Math.random() * 6) + 1;
-  // console.log(randomNumber);
-
   curBox -= randomNumber;
+  if (curBox < 0) {
+    curBox = 0;
+  }
   arr[curBox].appendChild(pawn);
-  console.log(curBox);
-  console.log(randomNumber);
+  console.log("New curBox:", curBox);
+  console.log("Rolled:", randomNumber);
 };
 
-// button.onclick = function () {
-//   for (let i = 0; i < arr.length; i++) {
-//     arr[i].style.border = "none";
-//     let pawn = arr[i].querySelector(".pawn");
-//     if (pawn) {
-//       arr[i].removeChild(pawn);
-//     }
-//   }
-
-//   if (count > 0) {
-//     count--;
-//     let currentBox = arr[count];
-
-//     if (currentBox) {
-//       currentBox.style.border = "2px solid white";
-
-//       currentBox.appendChild(pawn);
-//       // currentBox.innerText = " "
-//     }
-//   }
-
-// };
+// Reset the game
 start.onclick = function () {
   location.reload();
 };
-
-// button.onclick = function () {
-//   count = count+1;
-//   console.log(count)
-//   if (boxes === 2) {
-//     let pawn = document.createElement("div");
-//     pawn.className = "pawn";
-//     boxes.appendChild(pawn);
-//   }
-// };
-// if (
-//   (count >= 11 && count <= 20) ||
-//   (count >= 31 && count <= 40) ||
-//   (count >= 51 && count <= 60) ||
-//   (count >= 71 && count <= 80) ||
-//   (count >= 91 && count <= 100)
-// ) {
-// }
-// else {
-//   // store.appendChild(boxes);
-//   // boxes.style.border = "2px solid white";
-
-// }
