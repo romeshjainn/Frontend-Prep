8d04a2f6e1ba4d0dd2ae410d7ab8aab1eb679b75
romeshjainn <romeshjainofficial@gmail.com>
2023-08-13 10:45:24 +0530

    update

--- Snake & Ladders/script.js
+++ Snake & Ladders/script.js
@@ -1,16 +1,20 @@
 let container = document.createElement("div");
+let button = document.createElement("button");
 container.className = "container";
 document.body.appendChild(container);
+document.body.appendChild(button);
+button.innerText = "button";
 document.body.className = "gridCenter";
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
-
+let boxes;
 let store = document.createElement("div");
 container.appendChild(store);
+let boxCount = 0;
+let arr = [];
 let count = 0;
-
 for (let i = 100; i > 0; i--) {
-  let boxes = document.createElement("div");
+  boxes = document.createElement("div");
   container.appendChild(boxes);
   boxes.style.height = "10%";
   boxes.style.width = "10%";
@@ -18,38 +22,58 @@ for (let i = 100; i > 0; i--) {
   boxes.style.backgroundColor = "red";
   boxes.className = "gridCenter";
   boxes.style.color = "white";
-
   boxes.innerText = i;
-  count = 100 - i + 1;
+  boxCount = 100 - i + 1;
+  arr.push(boxes);
 
   if (
-    count <= 10 ||
-    (count > 20 && count <= 30) ||
-    (count > 40 && count <= 50) ||
-    (count > 60 && count <= 70) ||
-    (count > 80 && count <= 90)
+    boxCount <= 10 ||
+    (boxCount > 20 && boxCount <= 30) ||
+    (boxCount > 40 && boxCount <= 50) ||
+    (boxCount > 60 && boxCount <= 70) ||
+    (boxCount > 80 && boxCount <= 90)
   ) {
-    if (count % 2 == 0) {
+    if (boxCount % 2 == 0) {
       container.style.justifyContent = "row-reverse";
       boxes.style.backgroundColor = "blue";
     }
   } else {
-    if (count % 2 !== 0) {
+    if (boxCount % 2 !== 0) {
       boxes.style.backgroundColor = "blue";
     }
   }
 }
+console.log(arr);
+count = arr.length;
 
-  // if (
-  //   (count >= 11 && count <= 20) ||
-  //   (count >= 31 && count <= 40) ||
-  //   (count >= 51 && count <= 60) ||
-  //   (count >= 71 && count <= 80) ||
-  //   (count >= 91 && count <= 100)
-  // ) {
-  // }
-  // else {
-  //   // store.appendChild(boxes);
-  //   // boxes.style.border = "2px solid white";
+button.onclick = function () {
+  count--;
+  console.log(boxCount);
+  let currentBox = arr[count];
 
-  // }
+  if (currentBox) {
+    currentBox.style.border = "2px solid white";
+  }
+};
+// button.onclick = function () {
+//   count = count+1;
+//   console.log(count)
+//   if (boxes === 2) {
+//     let pawn = document.createElement("div");
+//     pawn.className = "pawn";
+//     boxes.appendChild(pawn);
+//   }
+// };
+// if (
+//   (count >= 11 && count <= 20) ||
+//   (count >= 31 && count <= 40) ||
+//   (count >= 51 && count <= 60) ||
+//   (count >= 71 && count <= 80) ||
+//   (count >= 91 && count <= 100)
+// ) {
+// }
+// else {
+//   // store.appendChild(boxes);
+//   // boxes.style.border = "2px solid white";
+
+// }
